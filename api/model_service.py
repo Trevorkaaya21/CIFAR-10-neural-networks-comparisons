4cddf1392599dbd1dffad939110c14c6fa6bea70
Trevorkaaya21 <t_kkaaya@salemstate.edu>
2026-09-22 23:20:59 +0000

    prepare SageMaker client for AWS deployment

--- api/model_service.py
+++ api/model_service.py
@@ -1,6 +1,7 @@
 
 import io
 import json
+import os
 import time
 
 import boto3
@@ -13,16 +14,28 @@ CLASS_NAMES = [
     "dog", "frog", "horse", "ship", "truck"
 ]
 
-ENDPOINT_NAME = "cifar10-serverless-endpoint"
-AWS_REGION = "us-west-2"
+ENDPOINT_NAME = os.getenv(
+    "SAGEMAKER_ENDPOINT_NAME",
+    "cifar10-serverless-endpoint"
+)
+
+AWS_REGION = os.getenv(
+    "AWS_REGION",
+    "us-west-2"
+)
 
 
 class ModelService:
-    def __init__(self):
-        self.runtime = boto3.client(
-            "sagemaker-runtime",
-            region_name=AWS_REGION
-        )
+    def __init__(self, runtime=None):
+        self.runtime = runtime
+
+    def _get_runtime(self):
+        if self.runtime is None:
+            self.runtime = boto3.client(
+                "sagemaker-runtime",
+                region_name=AWS_REGION
+            )
+        return self.runtime
 
     def preprocess_image(self, image_bytes):
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
@@ -47,7 +60,7 @@ class ModelService:
 
         start_time = time.perf_counter()
 
-        response = self.runtime.invoke_endpoint(
+        response = self._get_runtime().invoke_endpoint(
             EndpointName=ENDPOINT_NAME,
             ContentType="application/json",
             Body=payload
